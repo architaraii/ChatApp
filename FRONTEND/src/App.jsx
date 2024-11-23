77d796b8fe9dd37ac0239dcf14ea75a306cc2038
Yashvi Agrawal15 <yashvi.agrawal@adypu.edu.in>
2024-11-23 19:24:53 +0530

    login Page

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { Routes, Route }  from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+
+      <Navbar/>
+
+      <Routes>
+        <Route  path="/" element = { <HomePage/> } />
+        <Route  path="/signup" element = { <SignUpPage/> } />
+        <Route  path="/login" element = { <LoginPage/> } />
+        <Route  path="/settings" element = { <SettingsPage/> } />
+        <Route  path="/profile" element = { <ProfilePage/> } />
+
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
